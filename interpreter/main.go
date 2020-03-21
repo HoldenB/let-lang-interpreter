55b815c224a3517d8064433467642bade903f455
Holden Babineaux <holden.bab@outlook.com>
2020-03-21 17:12:56 -0500

    Restructure package directories

---
+++ interpreter/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+////////////////////////////////////////////////////////////////
+
+func main() {
+	filename := "examples/example_4.let"
+
+	filebuffer, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	inputdata := string(filebuffer)
+	data := bufio.NewScanner(strings.NewReader(inputdata))
+	data.Split(bufio.ScanRunes)
+
+	lexer := NewLexer(data)
+	for {
+		if lexer.currTokenType == EOF {
+			lexer.EnqueueToken(Token{EOF, "EOF"})
+			break
+		}
+		lexer.Lex()
+	}
+
+	for _, data := range lexer.tokenQueue {
+		fmt.Printf("Token: %d | Lexeme: %s\n", data.tokenType, data.tokenValue)
+	}
+
+	println()
+	root := ParseTokenStream(lexer.tokenQueue)
+	println()
+	PrintTreeBasic(root)
+	println()
+	PrintTree(root)
+}
