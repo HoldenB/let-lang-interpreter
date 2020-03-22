f78c84b8e5231d0246f7d3574010c8b52c8452e3
Holden Babineaux <holden.bab@outlook.com>
2020-03-21 22:00:00 -0500

    Added base evaluator code

--- interpreter/let_evaluator.go
+++ interpreter/let_evaluator.go
@@ -1,1 +1,55 @@
 package main
+
+// Binding represents a pairing of a variable and a value
+type Binding struct {
+	varName string
+	value   string
+}
+
+////////////////////////////////////////////////////////////////
+
+// PushBinding -
+func PushBinding(bindings []Binding, b Binding) []Binding {
+	newBindings := []Binding{b}
+	bindings = append(newBindings, bindings...)
+
+	return bindings
+}
+
+// Lookup -
+func Lookup(bindings []Binding, varName string) string {
+	for i := range bindings {
+		if bindings[i].varName == varName {
+			return bindings[i].value
+		}
+	}
+
+	return ""
+}
+
+////////////////////////////////////////////////////////////////
+
+// Evaluator -
+type Evaluator struct {
+	astRoot  *AstNode
+	bindings []Binding
+}
+
+// PushBinding -
+func (e *Evaluator) PushBinding(b Binding) {
+	e.bindings = PushBinding(e.bindings, b)
+}
+
+// Lookup -
+func (e *Evaluator) Lookup(varName string) string {
+	return Lookup(e.bindings, varName)
+}
+
+// Evaluate -
+func (e *Evaluator) Evaluate() string {
+	return e.evaluate(e.astRoot)
+}
+
+func (e *Evaluator) evaluate(localParent *AstNode) string {
+	return ""
+}
